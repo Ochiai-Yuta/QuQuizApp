fd50ac874617480cb8000ec0b1526a351210e498
Ochiai-Yuta <purpleballoon@outlook.jp>
2021-07-13 16:40:45 +0900

    試作レイアウト

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView label = this.findViewById(R.id.label);
+        label.setText(R.string.text);
     }
 }
