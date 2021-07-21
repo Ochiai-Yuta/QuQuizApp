cda0427b1946499fc101316b990b0d35a679b96d
Ochiai-Yuta <purpleballoon@outlook.jp>
2021-07-21 12:10:30 +0900

    Layoutを指定されたとおりにした

--- app/src/main/java/com/example/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -9,7 +9,9 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,43 +21,45 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
     }
 
-    public void tapCircleButton(View view){
-        //judgeViewの子ビューをすべて非表示にする
-        FrameLayout frame = findViewById(R.id.judgeView);
 
-        ImageFilterView tmp;
-
-        for(int i=0; i<frame.getChildCount(); i++){
-            tmp = (ImageFilterView)frame.getChildAt(i);
-            tmp.setVisibility(View.INVISIBLE);
-        }
-
-        //マルを表示
-        ImageFilterView CircleImage = findViewById(R.id.circleImage);
-        CircleImage.setVisibility(View.VISIBLE);
-
-        TextView quizText = findViewById(R.id.quizText);
-        quizText.setText("まる");
-    }
-
-    public void tapCrossButton(View view){
-        //judgeViewの子ビューをすべて非表示にする
-        FrameLayout frame = findViewById(R.id.judgeView);
-
-        ImageFilterView tmp;
-
-        for(int i=0; i<frame.getChildCount(); i++){
-            tmp = (ImageFilterView)frame.getChildAt(i);
-            tmp.setVisibility(View.INVISIBLE);
-        }
-
-        //バツを表示
-        ImageFilterView CrossImage = findViewById(R.id.crossImage);
-        CrossImage.setVisibility(View.VISIBLE);
-
-        TextView quizText = findViewById(R.id.quizText);
-        quizText.setText("ばつ");
-    }
+//    public void tapCircleButton(View view){
+//        //judgeViewの子ビューをすべて非表示にする
+//        FrameLayout frame = findViewById(R.id.judgeView);
+//
+//        ImageFilterView tmp;
+//
+//        for(int i=0; i<frame.getChildCount(); i++){
+//            tmp = (ImageFilterView)frame.getChildAt(i);
+//            tmp.setVisibility(View.INVISIBLE);
+//        }
+//
+//        //マルを表示
+//        ImageFilterView CircleImage = findViewById(R.id.circleImage);
+//        CircleImage.setVisibility(View.VISIBLE);
+//
+//        TextView quizText = findViewById(R.id.quizText);
+//        quizText.setText("まる");
+//    }
+//
+//    public void tapCrossButton(View view){
+//        //judgeViewの子ビューをすべて非表示にする
+//        FrameLayout frame = findViewById(R.id.judgeView);
+//
+//        ImageFilterView tmp;
+//
+//        for(int i=0; i<frame.getChildCount(); i++){
+//            tmp = (ImageFilterView)frame.getChildAt(i);
+//            tmp.setVisibility(View.INVISIBLE);
+//        }
+//
+//        //バツを表示
+//        ImageFilterView CrossImage = findViewById(R.id.crossImage);
+//        CrossImage.setVisibility(View.VISIBLE);
+//
+//        TextView quizText = findViewById(R.id.quizText);
+//        quizText.setText("ばつ");
+//    }
 }
