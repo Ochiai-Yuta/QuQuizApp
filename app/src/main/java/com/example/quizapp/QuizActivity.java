5aaac4a8f2d94fc0ade4f76fde77a8b14bf1a08e
Ochiai-Yuta <purpleballoon@outlook.jp>
2021-07-22 23:58:25 +0900

    レイアウトを修正

--- app/src/main/java/com/example/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -16,12 +16,35 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    public static String SELECT_MESSAGE = "com.example.quizapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+    }
+
+    /**丸ボタンをタップされたときの挙動**/
+    public void tapCircleButton(View view){
+        //メッセージの初期化
+        SELECT_MESSAGE = "";
+        //丸の選択を送る
+        Intent intent = new Intent(this, JudgeActivity.class);
+        intent.putExtra(SELECT_MESSAGE, "circle");
+        //judge画面に切り替え
+        startActivity(intent);
+    }
+
+    /**バツボタンをタップされたときの挙動**/
+    public void tapCrossButton(View view){
+        //メッセージの初期化
+        SELECT_MESSAGE = "";
+        //丸の選択を送る
+        Intent intent = new Intent(this, JudgeActivity.class);
+        intent.putExtra(SELECT_MESSAGE, "cross");
+        //judge画面に切り替え
+        startActivity(intent);
     }
 
 
