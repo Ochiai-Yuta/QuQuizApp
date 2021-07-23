ace9abbdfd4c3e4443753f6a241a16180fa3693b
Ochiai-Yuta <purpleballoon@outlook.jp>
2021-07-23 16:59:59 +0900

    判定レイアウトの追加

--- app/src/main/java/com/example/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -1,22 +1,10 @@
 package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.utils.widget.ImageFilterView;
 
 import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Locale;
 
 public class QuizActivity extends AppCompatActivity {
     //マルバツの選択を格納する
