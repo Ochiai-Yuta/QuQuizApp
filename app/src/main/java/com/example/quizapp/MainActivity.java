a478bc2d5abd21db220d8036e073715899c195b6
Ochiai-Yuta <purpleballoon@outlook.jp>
2021-07-26 01:18:38 +0900

    クイズJSONの追加

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -20,16 +20,11 @@ public class MainActivity extends AppCompatActivity {
         //スタート画面のラベルに文字を入れる
         TextView label = this.findViewById(R.id.startLabel);
         label.setText(R.string.text);
+    }
 
-        //スタートボタンを押下した時の挙動
-        Button startButton = findViewById(R.id.startButton);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //画面の切り替え
-                Intent intent = new Intent(MainActivity.this, QuizActivity.class);
-                startActivity(intent);
-            }
-        });
+    public void tapStartButton(View view){
+        //画面の切り替え
+        Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        startActivity(intent);
     }
 }
